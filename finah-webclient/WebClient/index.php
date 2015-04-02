565e126cff540e96149d9926c892f527f9e60802
Kristof Spaas <kristof.spaas@student.pxl.be>
2015-04-02 21:04:20 +0200

    Responsive design for mobile

--- finah-webclient/WebClient/index.php
+++ finah-webclient/WebClient/index.php
@@ -11,7 +11,7 @@ $rapport_Id = @$_GET['rapportId'];
         <title>Welkom</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="opmaak.css"/>
+        <link rel="stylesheet" type="text/css" href="opmaakwelkom.css"/>
     </head>
     <body>
 
