f783d2d78e7c72b7519977f8fd5a46af2059afbc
Michael Voorjans <michael.voorjans@student.pxl.be>
2015-05-13 11:55:54 +0200

    PDF Class toegevoegd

--- finah-desktop-java/src/net/finah/GUI/JavaJFrameApplication.java
+++ finah-desktop-java/src/net/finah/GUI/JavaJFrameApplication.java
@@ -15,10 +15,12 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
 
 import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -35,18 +37,27 @@ public class JavaJFrameApplication {
         private JMenu fileMenu = new JMenu("Login");
         private JMenuItem inloggen = new JMenuItem("Inloggen");
         private JMenuItem uitloggen = new JMenuItem("Uitloggen");
-        private JTable patientTabel = new JTable(250,3);
+        private JTable patientTabel = new JTable(250,3){
+        	public boolean isCellEditable(int row, int column) {                
+                return false;               
+        }
+        };
+        /*
+        private JTable rapportTabel = new JTable(250,5){
+        	public boolean isCellEditable(int row, int column) {                
+                return false;               
+        }
+        };
+        */
         private JTable rapportTabel = new JTable(250,5);
         private JScrollPane patientPane=new JScrollPane(patientTabel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         private JScrollPane rapportPane =new JScrollPane(rapportTabel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         private JButton jButton = new JButton("Voeg patient toe");
+        private JButton button = new JButton("Test");
         
         public MyTabJPanel(){
             super(new BorderLayout());
             LoginPanel login = new LoginPanel();
-            
-           
-           
             
             JPanel jPaneA = new JPanel();
             patientTabel.getColumnModel().getColumn(0).setHeaderValue("Voornaam");
@@ -64,53 +75,35 @@ public class JavaJFrameApplication {
             
              
             JPanel jPaneB = new JPanel();
-            jPaneB.add(rapportPane);
             rapportTabel.getColumnModel().getColumn(0).setHeaderValue("Patient");
             rapportTabel.getColumnModel().getColumn(1).setHeaderValue("Mantelzorger");
             rapportTabel.getColumnModel().getColumn(2).setHeaderValue("Vragenlijst");
             rapportTabel.getColumnModel().getColumn(3).setHeaderValue("Datum");
-            rapportTabel.getColumnModel().getColumn(4).setHeaderValue("Bekijken");
+            rapportTabel.getColumnModel().getColumn(4).setHeaderValue("ID");
+            jPaneB.add(rapportPane);
+            jPaneB.add(button);
+            button.addActionListener(new ActionListener(){
+           	 @Override
+             public void actionPerformed(ActionEvent e) {
+         		int rij = rapportTabel.getSelectedRow();
+         		int id= Integer.parseInt( rapportTabel.getValueAt(rij, 4).toString());
+         		PDF.test(id);
+         		//JOptionPane.showMessageDialog(null,rapportTabel.getModel().getValueAt(rij,4));
+             }
+        });
+            
              
             JPanel jPaneC = new JPanel();
             JButton buttonExit = new JButton("Exit button on Tab C");
             buttonExit.addActionListener(new ActionListener(){
- 
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                	PDDocument document = new PDDocument();
-                	PDPage page = new PDPage();
-                	document.addPage( page );
 
-                	// Create a new font object selecting one of the PDF base fonts
-                	PDFont font = PDType1Font.HELVETICA_BOLD;
-
-                	// Start a new content stream which will "hold" the to be created content
-                	PDPageContentStream contentStream;
-					try {
-						contentStream = new PDPageContentStream(document, page);
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
 					
-
-                	// Define a text content stream using the selected font, moving the cursor and drawing the text "Hello World"
-                	contentStream.beginText();
-                	contentStream.setFont( font, 40 );
-                	contentStream.moveTextPositionByAmount( 100, 700 );
-                	contentStream.drawString( "Hello World" );
-                	contentStream.endText();
-
-                	// Make sure that the content stream is closed:
-                	contentStream.close();
-
-                	// Save the results and ensure that the document is properly closed:
-                	document.save( "Hello World.pdf");
-                	document.close();
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					} catch (COSVisitorException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-                }
+				}
+ 
+                
  
             });
             
@@ -125,7 +118,7 @@ public class JavaJFrameApplication {
              
             add(menuBar,BorderLayout.NORTH);
             add(jTabbedPane,BorderLayout.CENTER);
-            
+     
             
         }
          
---
+++ finah-desktop-java/src/net/finah/GUI/PDF.java
@@ -0,0 +1,50 @@
+package net.finah.GUI;
+
+import java.io.IOException;
+
+import org.apache.pdfbox.exceptions.COSVisitorException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
+public class PDF {
+	public static void test(int ID) {
+	PDDocument document = new PDDocument();
+	PDPage page = new PDPage();
+	document.addPage( page );
+	String tekst= Integer.toString(ID);
+
+	// Create a new font object selecting one of the PDF base fonts
+	PDFont font = PDType1Font.HELVETICA_BOLD;
+
+	// Start a new content stream which will "hold" the to be created content
+	PDPageContentStream contentStream;
+	try {
+		contentStream = new PDPageContentStream(document, page);
+	
+
+	// Define a text content stream using the selected font, moving the cursor and drawing the text "Hello World"
+	contentStream.beginText();
+	contentStream.setFont( font, 40 );
+	contentStream.moveTextPositionByAmount( 100, 700 );
+	contentStream.drawString(tekst );
+	contentStream.endText();
+
+	// Make sure that the content stream is closed:
+	contentStream.close();
+
+	// Save the results and ensure that the document is properly closed:
+	document.save( "Hello World.pdf");
+	document.close();
+	} catch (IOException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	} catch (COSVisitorException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+}
+
+}
